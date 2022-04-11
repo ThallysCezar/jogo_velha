546eda262f3bd7d8cd38380e94e6238b008ccba9
Thallys <thalys_cezar@hotmail.com>
2022-04-11 20:01:55 -0300

    refactor: configurando os arquivos do javascript

---
+++ script/script.js
@@ -0,0 +1,10 @@
+const xClass = 'x';
+const oClass = 'o';
+let xTurn;
+
+const celulas = document.querySelectorAll('.celulas');
+const borda = document.querySelectorAll('#borda');
+
+const gamEndMessage = document.querySelectorAll('[data-game-end-message]');
+
+const gamEndElement = document.querySelectorAll('#gameEndMessage');
