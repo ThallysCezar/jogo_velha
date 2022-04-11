3fdc62d5b94b5d4965eb4f0eaf5818788d45ab8a
Thallys <thalys_cezar@hotmail.com>
2022-04-11 20:19:47 -0300

    refactor: configurando os arquivos do javascript, parte 3

--- script/script.js
+++ script/script.js
@@ -3,13 +3,15 @@ const oClass = 'o';
 let xTurn;
 
 const celulas = document.querySelectorAll('.celulas');
-const borda = document.querySelectorAll('#borda');
+const borda = document.getElementById('borda');
 
 const gamEndMessage = document.querySelectorAll('[data-game-end-message]');
 
 const gamEndElement = document.querySelectorAll('#gameEndElement');
 
 const restartButton = document.querySelectorAll('#restartButton');
+
+startGame()
 
 function startGame() {
     xTurn = true;
